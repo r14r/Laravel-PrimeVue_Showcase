212d8e18e3d7433c1448d97b0ed1b7ef83831918
Ralph Göstenmeier <r14r@via-internet.de>
2026-04-10 10:36:31 +0200

    'changes'

---
+++ .set_env.sh
@@ -0,0 +1,10 @@
+export ENV_NAME=node
+export ENV_HOME=$PWD
+export ENV_VSCODE=primefaces_primevue_showcase
+
+. venv env    init
+. venv vscode init
+
+. venv composer   init 
+. venv laravel    init 
+. venv node       init 24.13.0
